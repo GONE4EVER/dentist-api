972773ef0b4ba31fe1ee1aec63503bd1e6e29719
Nikita <mikita.ramanchuk@itechart-group.com>
2020-05-11 11:07:58 +0300

    [add] pipe helper

---
+++ src/utils/pipe.js
@@ -0,0 +1,16 @@
+/**
+ * Takes functions sequence and calls them in specified order.
+ * Each further function is being called with result of the previous one.
+ * Providing arguments for the first function in sequence is performed by calling
+ * the returned value with required ones.
+ * @param {Function[]} funcs
+ */
+
+const pipe = (...funcs) => funcs
+  .filter((func) => func instanceof Function)
+  .reduce(
+    (prev, next) => (...args) => next(prev(...args)),
+  );
+
+
+export default pipe;
