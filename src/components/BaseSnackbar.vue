b96cd3750288b39b3d361bb3987ff0672141857d
Nikita <mikita.ramanchuk@itechart-group.com>
2020-05-12 14:03:17 +0300

    [add] snackbar base component

---
+++ src/components/BaseSnackbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-snackbar
+    :value="visible"
+    :bottom="y === 'bottom'"
+    :left="x === 'left'"
+    :top="y === 'top'"
+    :right="x === 'right'"
+    :color="color"
+    :timeout="timeout"
+  >
+    {{ text }}
+    <v-btn
+      dark
+      text
+      @click="close"
+    >
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+const X_AXIS = [ 'right', 'left' ];
+const Y_AXIS = [ 'top', 'bottom' ];
+
+const DEFAULT_TIMEOUT = 3000;
+
+
+export default {
+  props: {
+    x: {
+      type: String,
+      validator(value) {
+        return X_AXIS.includes(value);
+      },
+    },
+    y: {
+      type: String,
+      validator(value) {
+        return Y_AXIS.includes(value);
+      },
+    },
+    color: {
+      type: String,
+      default: undefined,
+    },
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    timeout: {
+      type: Number,
+      default: DEFAULT_TIMEOUT,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('update:visible', false);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/registerComponents.js
+++ src/registerComponents.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 
 import BaseDatatable from 'components/BaseDatatable.vue';
+import BaseSnackbar from 'components/BaseSnackbar.vue';
 
 
 Vue.component('base-datatable', BaseDatatable);
+Vue.component('base-snackbar', BaseSnackbar);
