7214b13db1d2ddaf5849220ba0c1b3d6c9baadf2
Nikita <mikita.ramanchuk@itechart-group.com>
2020-05-14 14:56:48 +0300

    [add] added availability prop to doctor entity

--- src/entities/Doctor.entity.js
+++ src/entities/Doctor.entity.js
@@ -1,4 +1,23 @@
 import Person from 'entities/Person';
 
+import formatters from 'utils/formatters';
 
-export default class Doctor extends Person {}
+
+export default class Doctor extends Person {
+  constructor(props) {
+    const {
+      availability,
+      ...rest
+    } = props;
+
+    super(rest);
+
+    this.availability = availability.map(
+      (date) => ({
+        date: formatters.shortDate.format(date),
+        time: formatters.time.format(date),
+        isoDate: formatters.isoFormat(formatters.shortDate)(date),
+      }),
+    );
+  }
+}
