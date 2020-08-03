440944f70e2415ee6d48a1ca99b0c79fae9510d0
Nikita Romanchuk <nromanchuk@Nikitas-MacBook-Pro.local>
2020-08-03 15:37:11 +0300

    [add] base provider class

---
+++ frontend/src/common/providers/Base.provider.js
@@ -0,0 +1,13 @@
+export default class BaseProvider {
+  constructor(Entity) {
+    this.Entity = Entity;
+  }
+
+  getMapped = (data) => (Array.isArray(data)
+    ? this.transformArray(data)
+    : this.transformObject(data))
+
+  transformArray = (data) => data.map(this.transformObject)
+
+  transformObject = (data) => new this.Entity(data)
+}
